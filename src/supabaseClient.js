49a300c581a454e7b38c940be08ff13e09d301ea
Valentina Gonzalez <vpgonzalez@miuandes.cl>
2026-05-18 16:09:13 -0400

    variables de entorno

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://lnvkedxilfkswxefzupp.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxudmtlZHhpbGZrc3d4ZWZ6dXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwODc2NTgsImV4cCI6MjA5MzY2MzY1OH0.NXNZyjzf2RUkwhABn02E7UVl8yygKPHH4EG9x9YZ0u4'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
